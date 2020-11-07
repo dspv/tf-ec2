98a9dfb617529d5f8674f703b5edc95945e65c12
dspv <dmitriy@solodukha.com>
2020-11-07 17:12:54 +0200

    Fixes

--- main.tf
+++ main.tf
@@ -172,11 +172,6 @@ resource "aws_network_interface" "web-server-nic" {
   subnet_id       = aws_subnet.prod-subnet-1.id
   private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_web.id]
-
-  #  attachment {
-  #    instance     = aws_instance.test.id
-  #    device_index = 1
-  #  }
 }
 
 ### 8. Assign an EIP to the network interface create on step 7
