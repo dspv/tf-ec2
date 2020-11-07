dd9c6ebefb13d5c7bc93c3c78b9f6d006a32a0b9
dspv <dmitriy@solodukha.com>
2020-11-07 16:58:03 +0200

    t2.micro --> t2.small

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ variable "subnet_prefix" {
 
 variable "instance_type" {
   description = "EC2 Instance type"
-  default     = "t2.micro"
+  default     = "t2.small"
 }
 
 variable "instance_ami" {
